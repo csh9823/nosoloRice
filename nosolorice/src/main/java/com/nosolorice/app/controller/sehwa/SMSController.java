a5a70cf0d0a96b1c14f4c109de8717e9c1dc4a2b
oyoo525 <141537487+oyoo525@users.noreply.github.com>
2024-02-29 18:57:13 +0900

    Update SMSController.java
    CoolSMS key delete

--- nosolorice/src/main/java/com/nosolorice/app/controller/sehwa/SMSController.java
+++ nosolorice/src/main/java/com/nosolorice/app/controller/sehwa/SMSController.java
@@ -22,7 +22,7 @@ public class SMSController {
 
     public SMSController() {
         // 반드시 계정 내 등록된 유효한 API 키, API Secret Key를 입력해주셔야 합니다!
-        this.messageService = NurigoApp.INSTANCE.initialize("NCSJVKSXY5RG42XZ", "S21PRXKAHV1CLHYZPEAAAUAHBPXNJBAY", "https://api.coolsms.co.kr");
+        this.messageService = NurigoApp.INSTANCE.initialize("", "", "https://api.coolsms.co.kr");
     }
 	
 	@RequestMapping(value="/verifyNumSMS.ajax", method=RequestMethod.POST)
